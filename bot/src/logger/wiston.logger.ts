49c8b4911833cd13436ecc78307e542c59b36df3
crhistian.vergara <krisskira@gmail.com>
2021-11-26 19:38:22 -0500

    refactor logger files

--- bot/src/logger/wiston.logger.ts
+++ bot/src/logger/wiston.logger.ts
@@ -1,76 +1,73 @@
 import { join } from "path";
 import { Environment } from "../interfaces/app.interface";
 import { createLogger, format, Logger, transports } from "winston";
-import { iLogger, iLoggerArgs } from "../interfaces/logger.interface";
+import { ErrorType, iLogger, iLoggerArgs } from "../interfaces/logger.interface";
 
 export class WistonLogger implements iLogger {
     env: Environment;
-    private _logger: Logger;
+    private loggers: Record<ErrorType, Logger>;
 
     constructor(env: Environment) {
         this.env = env;
-        const transporOptionBuilder = (level: string) => {
-            return {
-                dirname: join(__dirname, "../../logs"),
-                filename: level + ".log",
-                level: level,
-                maxsize: 5 * 1024 * 1024,
-                maxFiles: 5,
-                format: format.combine(
-                    format.timestamp(),
-                    format.printf(
-                        ({ level, message, timestamp }) =>
-                            `[${timestamp}] [${level}]:\n${message}\n`
-                    ),
-                ),
-            };
-        };
-        this._logger = createLogger({
-            level: "debug",
-            format: format.combine(format.simple()),
-            transports: [
-                new transports.File(transporOptionBuilder("error")),
-                new transports.File(transporOptionBuilder("warn")),
-                new transports.File(transporOptionBuilder("info")),
-            ],
-        });
+        const levels: ErrorType[] = [
+            "INFO", "WARNING", "ERROR"
+        ];
+
+        this.loggers = levels.reduce((obj, level) => ({
+            ...obj,
+            [level]: createLogger({
+                transports: [
+                    new transports.File(this.transporOptionBuilder(level)),
+                ],
+            })
+        }), {} as Record<ErrorType, Logger>);
 
         if (env !== "production") {
-            this._logger.add(
-                new transports.Console({
-                    level: "debug",
-                    format: format.combine(format.colorize(), format.simple()),
-                })
-            );
+            this.loggers["DEBUG"] = createLogger({
+                level: "debug",
+                transports: [
+                    new transports.File(this.transporOptionBuilder("DEBUG")),
+                    new transports.Console({
+                        format: format.combine(
+                            format.timestamp(),
+                            format.colorize(),
+                            format.printf(
+                                ({ level, message, timestamp, tag = "" }) =>
+                                    `[${timestamp}] [${level}]: \x1b[1m${tag}\x1b[0m\n` +
+                                    `\x1b[2m${message}\x1b[0m\n`
+                            ),
+                        ),
+                    }),
+                ],
+            });
         }
     }
 
     log(args: iLoggerArgs): void {
-        switch (args.type) {
-        case "DEBUG":
-            this._logger.log({
-                level: "debug",
-                message: args.tag + ": " + args.msg,
-            });
-            break;
-        case "INFO":
-            this._logger.log({
-                level: "info",
-                message: args.tag + ": " + args.msg,
-            });
-            break;
-        case "WARNING":
-            this._logger.log({
-                level: "warn",
-                message: args.tag + ": " + args.msg,
-            });
-            break;
-        case "ERROR":
-            this._logger.log({
-                level: "error",
-                message: args.tag + ": " + args.msg,
-            });
-            break;
-        }
+        const level = args.type || "DEBUG";
+        this.loggers[level].log({
+            level: level.toLowerCase(),
+            message: args.msg,
+            tag: args.tag,
+        });
+    }
+
+    private transporOptionBuilder(level: ErrorType): transports.FileTransportOptions {
+        return {
+            level: level.toLowerCase(),
+            filename: level.toLowerCase() + ".log",
+            dirname: join(__dirname, "../../logs"),
+            maxsize: 5 * 1024 * 1024,
+            maxFiles: 5,
+            tailable: true,
+            format: format.combine(
+                format.timestamp(),
+                format.printf(
+                    ({ level, message, timestamp, tag = "" }) =>
+                        `[${timestamp}] [${level}]: ${tag}` +
+                        `\n${message}\n`
+                ),
+            ),
+        };
     }
 }
